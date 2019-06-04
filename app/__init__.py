27890a16158ca875336191cee8713381efadbe45
simon <simonnicholls1@googlemail.com>
2019-06-04 23:34:16 +0100

    new

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from os import os
 
 
 app = Flask(__name__)
@@ -6,3 +7,7 @@ app = Flask(__name__)
 
 from app import routes
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
