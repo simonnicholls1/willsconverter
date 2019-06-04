a30f4263fd13a1a30e609e5245dd666d3508e2e0
simon <simonnicholls1@googlemail.com>
2019-06-04 23:36:57 +0100

    new

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,4 @@ from app import routes
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
