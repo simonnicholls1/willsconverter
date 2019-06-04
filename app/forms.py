84bc036419b5ec4d111a0c1c5be273ee22744bdb
simon <simonnicholls1@googlemail.com>
2019-06-05 00:05:26 +0100

    new

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
-
 class ConverterForm(FlaskForm):
     types = RadioField('Type', choices=[('Kgs', 'Kgs'), ('Lbs', 'Lbs')])
     value = StringField('Value', validators=[DataRequired()])
